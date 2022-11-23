c8ed742d6e4703f13a04b0e309cd5457234c7f8d
Augusto Mancini <augustomancini00@gmail.com>
2022-11-23 16:56:12 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioLista/Models/Employess.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioLista.Models
+{
+    public class Employess
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; private set; }
+
+
+        public Employess(int iD, string name, double salary)
+        {
+            ID = iD;
+            Name = name;
+            Salary = salary;
+        }
+
+     
+        public void  IncreaseSalary(double percentage)
+        {
+             Salary += Salary *percentage / 100;
+        }
+
+       
+
+    }
+}
---
+++ ExercicioLista/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using ExercicioLista.Models;
+
+namespace ListaDeEmpregados
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("How many employees do you will Registered? : ");
+            int employess = int.Parse(Console.ReadLine());
+            List<Employess> listOfEmployees = new List<Employess>();
+            Console.WriteLine("");
+
+            for (int i = 1; i < employess; i++)
+            {
+                Console.WriteLine("========================================");
+                Console.WriteLine($"Employee #{i}");
+                Console.Write("What is the employe's ID? :");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("What is the name of the Employee? :");
+                string name = Console.ReadLine();
+                Console.Write("Whats it his Salary? : ");
+                double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                listOfEmployees.Add(new Employess(id, name, salary));
+            }
+
+            Console.WriteLine("");
+            Console.Write("Enter the id that will have a salary increase : ");
+            int searchId = int.Parse(Console.ReadLine());
+
+            Employess search = listOfEmployees.Find(x => x.ID == searchId);
+            if (search != null)
+            {
+                Console.Write("Enter the percentage: ");
+                double percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                search.IncreaseSalary(percentage);
+            }
+            Console.WriteLine("");
+           else
+            {
+                Console.WriteLine("Thats Id does no exists");
+            }
+
+            foreach (Employess obj in listOfEmployees)
+            {
+                 
+                Console.WriteLine("Upadate list of employess :");
+                Console.WriteLine($"ID: {obj.ID}");
+                Console.WriteLine($"Name: {obj.Name}");
+                Console.WriteLine($"Salary: {obj.Salary}");
+            }
+        }
+    }
+}
